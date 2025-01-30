28c80184d067d4dc13295350b824d40cfd970608
Frank-en-stein <faisal.rahman@trili.tech>
2025-01-30 23:47:44 +0600

    SnapshotBlock() fix (#23)

--- tezos/params.go
+++ tezos/params.go
@@ -258,14 +258,18 @@ func (p *Params) IsSnapshotBlock(height int64) bool {
 
 func (p *Params) SnapshotBlock(cycle int64, index int) int64 {
 	// adjust to target cycle
+	if p.Version > 18 {
+		index = 15
+	}
 	at := p.AtCycle(cycle)
 	base := at.SnapshotBaseCycle(cycle)
+	baseBlocksPerSnapshot := p.AtCycle(base).BlocksPerSnapshot
 	if base < 0 {
 		return 0
 	}
-	offset := int64(index+1) * at.BlocksPerSnapshot
-	if offset > at.BlocksPerCycle {
-		offset = at.BlocksPerCycle
+	offset := int64(index+1) * baseBlocksPerSnapshot
+	if offset > baseBlocksPerSnapshot {
+		offset = baseBlocksPerSnapshot
 	}
 	return at.CycleStartHeight(base) + offset - 1
 }
