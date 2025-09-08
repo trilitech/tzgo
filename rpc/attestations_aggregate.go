85ccf729fdc70f6ea9e127f403702de75ee0b532
huancheng-trili <168652455+huancheng-trili@users.noreply.github.com>
2025-09-08 10:01:51 +0100

    feat: implement operation preattestations_aggregate (#32)

--- rpc/attestations_aggregate.go
+++ rpc/attestations_aggregate.go
@@ -24,3 +24,13 @@ type Committee struct {
 	Slot           int     `json:"slot"`            // v023+
 	DalAttestation tezos.Z `json:"dal_attestation"` // v023+
 }
+
+// Ensure PreattestationsAggregate implements the TypedOperation interface.
+var _ TypedOperation = (*PreattestationsAggregate)(nil)
+
+// PreattestationsAggregate represents an pre-attestations aggregate operation.
+type PreattestationsAggregate struct {
+	Generic
+	ConsensusContent ConsensusContent `json:"consensus_content"` // v023+
+	Committee        []int            `json:"committee"`         // v023+
+}
