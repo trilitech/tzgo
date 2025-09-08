87f2f92f2f4fdab1a51879a1eb0e9f737aab1323
huancheng-trili <168652455+huancheng-trili@users.noreply.github.com>
2025-09-08 09:41:11 +0100

    feat: implement operation attestations_aggregate (#31)

---
+++ rpc/attestations_aggregate.go
@@ -0,0 +1,26 @@
+package rpc
+
+import (
+	"github.com/trilitech/tzgo/tezos"
+)
+
+// Ensure AttestationsAggregate implements the TypedOperation interface.
+var _ TypedOperation = (*AttestationsAggregate)(nil)
+
+// AttestationsAggregate represents an attestations aggregate operation.
+type AttestationsAggregate struct {
+	Generic
+	ConsensusContent ConsensusContent `json:"consensus_content"` // v023+
+	Committee        []Committee      `json:"committee"`         // v023+
+}
+
+type ConsensusContent struct {
+	Level       int64             `json:"level"`              // v023+
+	Round       int               `json:"round"`              // v023+
+	PayloadHash tezos.PayloadHash `json:"block_payload_hash"` // v023+
+}
+
+type Committee struct {
+	Slot           int     `json:"slot"`            // v023+
+	DalAttestation tezos.Z `json:"dal_attestation"` // v023+
+}
