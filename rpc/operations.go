f11bb101672222ceeb201abb9eff3161d10bf830
huancheng-trili <168652455+huancheng-trili@users.noreply.github.com>
2026-01-21 19:38:54 +0000

    feat(rpc): capture address_registry_diff in operation metadata (#85)
    Co-authored-by: Vitalii Nazarov <vitalii.nazarov@trili.tech>

--- rpc/operations.go
+++ rpc/operations.go
@@ -110,6 +110,12 @@ type ConsensusPowerV024 struct {
 	BakingPower int64 `json:"baking_power,string"` // v024+
 }
 
+// AddressRegistryDiff represents the addresses that have been newly indexed through the opcode INDEX_ADDRESS.
+type AddressRegistryDiff struct {
+	Address tezos.Address `json:"address"`
+	Index   int           `json:"index,string"`
+}
+
 // OperationMetadata contains execution receipts for successful and failed
 // operations.
 type OperationMetadata struct {
@@ -169,6 +175,9 @@ type OperationResult struct {
 
 	// v019 DAL
 	DalResult
+
+	// v024+; only when the operation involves opcode INDEX_ADDRESS
+	AddressRegistryDiff *[]AddressRegistryDiff `json:"address_registry_diff"`
 }
 
 // Always use this helper to retrieve Ticket updates. This is because due to
